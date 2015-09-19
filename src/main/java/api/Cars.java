30c464f1c185e48503db518ed6bf4e2de4dd0ea9
agi290 <agi.okai@durham.ac.uk>
2015-09-19 12:37:31 +0100

    test

---
+++ src/main/java/api/Cars.java
@@ -0,0 +1,55 @@
+package api;
+
+import java.util.Date;
+
+public class Cars {
+
+
+private final long id;
+private String make;
+private String model;
+private int year;
+private Date entryDate;
+
+public Cars(long id){
+this.id =id;
+
+
+}
+
+
+public long getId() {
+return id;
+}
+
+public String getMake(){
+return make;
+}
+
+public void setMake(){
+this.make = make;
+}
+
+public String getModel(){
+return model;
+}
+
+public void setModel(){
+this.model = model;
+}
+
+public int getYear(){
+return year;
+}
+
+public void setYear(){
+this.year = year;
+}
+
+public Date getEntryDate(){
+return entryDate;
+}
+
+
+
+}
