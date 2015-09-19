bb1117457c85b86b4bd1cfd76119d037b8e31420
agi290 <agi.okai@durham.ac.uk>
2015-09-19 13:59:24 +0100

    final push

--- src/main/java/api/CarsController.java
+++ src/main/java/api/CarsController.java
@@ -9,18 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarsController {
 
 private AtomicLong count = new AtomicLong();
+private CarsService service;
 
 @RequestMapping("/cars")
 public Cars cars(@RequestParam(value="make", defaultValue="ford") String make, String model, int year, Date enrtyDate){
 return new Cars(count.incrementAndGet(), make, model, year, enrtyDate);
 
 }
+
+
 @RequestMapping( method = RequestMethod.POST )
    @ResponseStatus( HttpStatus.CREATED )
    @ResponseBody
-   public String create(Cars car){
-
-      return service.create(car);
+   public Long create( @RequestBody Cars car ){
+      Preconditions.checkNotNull( resource );
+      return service.create( car );
    }
 
+ public void update( @PathVariable( "id" ) Long id, @RequestBody Foo resource ){
+
+      service.update( car );
+   }
+
+@RequestMapping( value = "/{id}", method = RequestMethod.DELETE )
+   @ResponseStatus( HttpStatus.OK )
+   public void delete( @PathVariable( "id" ) Long id ){
+      service.deleteById( id );
+   }
 }
---
+++ src/main/java/api/CarsService.java
@@ -0,0 +1,13 @@
+package api;
+
+public CarsService{
+
+Cars create(Cars car){
+
+return crud.create(car);
+
+}
+
+
+
+}
